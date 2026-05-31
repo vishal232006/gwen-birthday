39112d5b6dc6835333c288622a331a643c5a1f67
vishal232006 <vishalsat23@gmail.com>
2026-06-01 00:39:54 +0530

    Add openStory function to display chapter 1

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Welcome Abeya Sara(Gwen)❤️");
+function openStory() {
+    document.querySelector(".book-cover").style.display = "none";
+    document.querySelector("#chapter1").style.display = "block";
+}
